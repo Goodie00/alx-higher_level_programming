d0019bc8a85e557ae0286ea2b3c723bbc21f8855
Goodie00 <goodieomolayo1@gmail.com>
2022-11-30 03:59:52 +0000

    Task 13

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -1,5 +1,6 @@
- #!/usr/bin/python3
-# Author - Goodness Omolayo
+#!/usr/bin/python3
+# 102-magic_calculation.py
+# Goodness Omolayo <goodieomolayo1@gmail.com>
 
 def magic_calculation(a, b, c):
     """Match bytecode provided by Holberton School."""
